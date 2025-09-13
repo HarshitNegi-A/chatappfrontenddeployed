9cca5008328f21b73c973cc65ed1eb95234f2066
Harshit Negi <harshit226@example.com>
2025-09-13 22:23:00 +0530

    signup,login

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,10 @@
-import { useState } from 'react'
-import './App.css'
+import SignUp from './components/Signup'
 
 function App() {
-  const [login, setLogin] = useState(false)
-
-  const handleLogin=()=>{
-    setLogin(!login)
-  }
-
+  
   return (
     <>
-      <form>
-        <h1>{login?"Login":"Signup"}</h1>
-        {!login && <><label htmlFor='name'>Name</label>
-        <input id='name' type='text' /></>}
-        <label htmlFor='email'>Email</label>
-        <input id='email' type='email' />
-        <label htmlFor='phno'>Phone Number</label>
-        <input id='phno' type='number' />
-        <label htmlFor='pass'>Password</label>
-        <input id='pass' type='password' />
-      </form>
-      <button onClick={handleLogin}>Already have a account? LogIn</button>
+      <SignUp/>
     </>
   )
 }
